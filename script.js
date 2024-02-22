fafd06d865f7c09ce32060501fb10b20bcbd5a74
Ilangasinghe I M H H <102861747+enghirushan@users.noreply.github.com>
2024-02-22 16:31:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//const keyword
+const username ={
+  name : "Haritha"
+}
+username.name = "pasan";
+console.log(username.name);
+
+
+
+//let keyword
+let Myname = "Saman";
+Myname = "Ajith";
+console.log(Myname);
+
+
+
+//var keyword
+var STUname="nadeeka";
+var STUname= "kamal";
+console.log(STUname);
